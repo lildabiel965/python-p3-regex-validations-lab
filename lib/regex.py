afe0a06cd701bfd985cd1f61d4ce8a8ff5c967aa
Daniel <daniel.mutembei@student.moringaschool.com>
2025-02-22 18:39:16 +0300

    “v.good”

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = re.compile(r'^[A-Z][a-z]*(?:[- \'][A-Za-z][a-z]*)*$')
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = re.compile(r"^(\(\d{3}\) \d{3}-\d{4}|\d{3}-\d{3}-\d{4}|\d{10})$")
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = re.compile(r"^[a-zA-Z][\w.]+@[a-zA-Z]+\.[a-zA-Z]+$")
 email_regex = re.compile(email_address)
